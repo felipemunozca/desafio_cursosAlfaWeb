d8bf4652dbc827357cd2c90d7db45cd18d2d9155
Felipe Muñoz <analista.felipe.m@gmail.com>
2023-08-09 22:23:56 -0400

    Se completa la vista de not found.

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,5 +1,19 @@
 <template>
-    <h1> PAGINA NO ENCONTRADA</h1>
+    <v-container class="error-container">
+        <v-row justify="center" align="center" class="error-message">
+            <div class="text-center">
+                <h1 class="display-2 font-weight-bold my-5">
+                    Pagina no encontrada
+                </h1>
+                <h4 class="display-1 my-5">
+                    La página que buscas no existe. Favor de volver a Home.
+                </h4>
+                <v-btn @click="redireccionar" class="mt-3" color="primary" elevation="5">
+                    Volver a Home
+                </v-btn>
+            </div>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
@@ -10,7 +24,11 @@ export default {
         return {}
     },
     // computed: {},
-    // methods: {}
+    methods: {
+        redireccionar() {
+            this.$router.push('/');
+        },
+    }
     // watch: {},
     // components: {},
     // mixins: [],
