0240893560e0758ae1ca4c555ce24ca3a80ad91c
Felipe Muñoz <analista.felipe.m@gmail.com>
2023-08-09 22:20:11 -0400

    Se completa la vista de home.

---
+++ src/components/CardCurso.vue
@@ -0,0 +1,94 @@
+<template>
+    <v-container>
+        <v-row justify="space-around">
+            <v-card width="350">
+
+                <v-img height="350px" :src="curso.img">
+                    <!-- <v-app-bar flat color="rgba(0, 0, 0, 0)" ></v-app-bar> -->
+                </v-img>
+
+                <v-card-text>
+                    <div class="font-weight-bold title black--text">
+                        {{ curso.nombre }}
+                    </div>
+                    <div class="font-weight-bold ml-8 mt-3 mb-2">
+                        Información
+                    </div>
+
+                    <v-timeline align-top dense>
+                        <v-timeline-item small color="purple">
+                            <div>
+                                <div class="font-weight-normal">
+                                    Costo: <strong>$ {{ curso.costo.toLocaleString('es-CL') }}</strong>
+                                </div>
+                                <div>
+                                    Duración: {{ curso.duracion }}
+                                </div>
+                            </div>
+                        </v-timeline-item>
+
+                        <v-timeline-item small color="green">
+                            <div>
+                                <div class="font-weight-normal">
+                                    Cupos: <strong>{{ curso.cupos }}</strong>
+                                </div>
+                                <div>
+                                    Completado: {{ curso.completado }}
+                                </div>
+                            </div>
+                        </v-timeline-item>
+
+                        <v-timeline-item small color="purple">
+                            <div>
+                                <div class="font-weight-normal">
+                                    Fecha de registro: {{ curso.fecha_registro }}
+                                </div>
+                            </div>
+                        </v-timeline-item>
+
+                        <v-timeline-item small color="green">
+                            <div>
+                                <div class="font-weight-normal">
+                                    Descripción: 
+                                </div>
+                                <div>
+                                    {{ curso.descripcion }}
+                                </div>
+                            </div>
+                        </v-timeline-item>
+                    </v-timeline>
+                </v-card-text>
+
+            </v-card>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'card-curso-comp',
+    props: {
+        curso: {
+            type: Object,
+            required: true,
+        }
+    },
+    data: function(){
+        return {
+
+        }
+    },
+    // computed: {},
+    // methods: {}
+    // watch: {},
+    // components: {},
+    // mixins: [],
+    // filters: {},
+    // -- Lifecycle Methods
+    // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped>
+    
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,53 @@
 <template>
-  <v-container>
+  <!-- <v-container>
     <h1 class="display-1 text-center my-4">Lista de Cursos</h1>
-  </v-container>
+    <v-row>
+      <v-col cols="12" md="4" lg="4">
+
+        <card-curso-comp></card-curso-comp>
+        <card-curso-comp></card-curso-comp>
+
+      </v-col>
+    </v-row>
+  </v-container> -->
+
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4" lg="4" v-for="curso in cursos" :key="curso.id">
+          <card-curso-comp :curso="curso"></card-curso-comp>
+          <!-- <card-curso-comp></card-curso-comp> -->
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
+import CardCurso from '@/components/CardCurso.vue';
+import { mapState } from 'vuex';
 
-  export default {
-    name: 'Home-view',
-
-    components: {
-    },
-  }
+export default {
+  name: 'Home-view',
+  // props: {},
+  data: function(){
+    return {}
+  },
+  computed: {
+    ...mapState(['cursos']),
+  },
+  // methods: {}
+  // watch: {},
+  components: {
+    'card-curso-comp': CardCurso,
+  },
+  // mixins: [],
+  // filters: {},
+  // -- Lifecycle Methods
+  // -- End Lifecycle Methods
+}
 </script>
+
+<style scoped>
+  
+</style>
